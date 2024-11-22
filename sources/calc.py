f3e272adcdd2278210bb3283a1a1b4f81d8f2e1e
Amal Bouazza <amal.bouazza@u-virtuelle.tn>
2024-11-22 21:00:45 +0100

    nouvelle version avec racine

--- sources/calc.py
+++ sources/calc.py
@@ -1,7 +1,7 @@
 """
 La librairie calc permet de faire les opérations basiques de calcul entre deux entiers.
 """
-
+import math
 def add(arg1,arg2):
     try:
         return int(arg1)+int(arg2)
@@ -34,8 +34,16 @@ def modulo(arg1,arg2):
         return int(arg1)%int(arg2)
     except ValueError:
         print("Un des arguments n'est pas un entier")    
-
-def ope(operateur,arg1,arg2):   
+def racine(arg1):
+    """Calcule la racine carrée d'un entier."""
+    try:
+        arg1 = int(arg1)
+        if arg1 < 0:
+            raise ValueError("Impossible de calculer la racine carrée d'un nombre négatif.")
+        return math.sqrt(arg1)
+    except ValueError as e:
+        print(e)
+def ope(operateur,arg1,arg2=None):   
     if operateur=='+':
         return add(arg1,arg2)        
     elif operateur=="%":
@@ -46,5 +54,7 @@ def ope(operateur,arg1,arg2):
         return mult(arg1,arg2)
     elif operateur=="/":
         return div(arg1,arg2)
+    elif operateur == "racine":
+        return racine(arg1)
     else:
         print("L'opérateur {} n'est pas reconnu.".format(operateur))
--- sources/prog.py
+++ sources/prog.py
@@ -1,6 +1,6 @@
 """
 Un script python prenant 3 arguments dont le premier est l'opération voulue et les deux suivants 
-les deux entiers 
+les deux entiers.
 """
 
 import calc
@@ -10,17 +10,27 @@ print("Bienvenue dans cette petite calculatrice sous Python pour entier.\n")
 
 
 def run_calc():
-    op = input("Choisissez une opération entre +, -, x , / et %      : \n")
-    term_1 = input("Entrer votre premier entier \n")
-    term_2 = input("Enter votre second entier \n")
-    res = calc.ope(op, term_1, term_2)
-    if res != None:
-        print("{} {} {} = {}".format(term_1, op, term_2, res))
-        keep_going = input(
-            "Voulez-vous stopper le programme ? Écrivez Oui, dans ce cas\n"
-        )
-        if keep_going == "Oui":
-            sys.exit(0)
+    op = input("Choisissez une opération entre +, -, x , /, %, et racine : \n")
+    
+    if op == "racine":
+        term_1 = input("Entrer l'entier pour calculer sa racine carrée : \n")
+        res = calc.ope(op, term_1)
+        if res is not None:
+            print(f"La racine carrée de {term_1} est {res:.2f}")
+    else:
+        term_1 = input("Entrer votre premier entier : \n")
+        term_2 = input("Entrer votre second entier : \n")
+        res = calc.ope(op, term_1, term_2)
+
+        if res is not None:
+            print(f"{term_1} {op} {term_2} = {res}")
+
+    keep_going = input("Voulez-vous stopper le programme ? Écrivez Oui pour arrêter.\n").strip()
+    if keep_going.lower() == "oui":
+        print("Merci d'avoir utilisé la calculatrice. À bientôt !")
+        sys.exit(0)
+    
+    # Recommencer le programme si l'utilisateur ne veut pas arrêter
     print("Nous allons recommencer le programme\n")
     run_calc()
 
